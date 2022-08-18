fc9a7539af14a4821ed8ab192c29ac0d3e353be5
Federico Mario <FedericoNMario@gmail.com>
2022-08-18 01:15:08 -0300

    Verify the entered character

--- hangman_game.py
+++ hangman_game.py
@@ -3,6 +3,21 @@ import random
 
 pressed_keys = []
 
+def enter_letter():
+    key_pressed = input('\nIngresa una letra: ').upper()
+    try:
+        if not key_pressed.isupper():
+            raise ValueError('Debe ingresar una letra')
+        if len(key_pressed) > 1:
+            raise Exception('No se puede ingresar mas de una letra')
+        return key_pressed
+    except ValueError as ve:
+        print(ve)
+        return False
+    except Exception as ex:
+        print(ex)
+        return False
+
 def run():
     with open(r'./files/data.txt', 'r', encoding='UTF-8') as f:
         word_list = [words.strip().upper() for words in f]
@@ -17,11 +32,16 @@ def run():
         print('Adivina la palabra!')
         for letter in word:
             print(letter, end=' ')
-        key_pressed = input('\nIngresa una letra: ').upper()
         
+        while True:
+            letter = enter_letter()
+            if letter:
+                break
+
         for pos, char in enumerate(choosen_word):
-            if (char == key_pressed):
+            if (char == letter):
                 word[pos] = char
+        letter = False
 
     os.system('clear')
     print('¡CORRECT! The word was: ' + choosen_word)
